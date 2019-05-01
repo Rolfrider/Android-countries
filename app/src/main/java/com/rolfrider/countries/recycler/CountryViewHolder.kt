0d557ea1bde2b6413f177101db332fa62849d36a
Rolfrider-Mac <ravkwiatkowski@gmail.com>
2019-05-01 19:21:08 +0200

    add downloading flag images

--- app/src/main/java/com/rolfrider/countries/recycler/CountryViewHolder.kt
+++ app/src/main/java/com/rolfrider/countries/recycler/CountryViewHolder.kt
@@ -1,7 +1,10 @@
 package com.rolfrider.countries.recycler
 
+import android.app.Activity
+import android.net.Uri
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.github.twocoffeesoneteam.glidetovectoryou.GlideToVectorYou
 import com.rolfrider.countries.CountryItem
 import kotlinx.android.synthetic.main.country_recycle_view_item.view.*
 
@@ -11,7 +14,9 @@ class CountryViewHolder(viewItem: View): RecyclerView.ViewHolder(viewItem){
 
     fun bindItem(countryItem: CountryItem, onItemClick: (CountryItem) -> Unit){
         nameView.text = countryItem.name
-        //TODO: Image download
+        val activity = itemView.context as Activity
+        GlideToVectorYou.
+            justLoadImage(activity, Uri.parse(countryItem.flagUrl), imageView)
         itemView.setOnClickListener { onItemClick(countryItem) }
     }
 }
