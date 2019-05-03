0bfa0b302cfd03b956a6368c48a29968da521dac
Rolfrider-Mac <ravkwiatkowski@gmail.com>
2019-05-03 11:09:39 +0200

    change to more testable architecture

--- app/src/main/java/com/rolfrider/countries/CountriesViewModel.kt
+++ app/src/main/java/com/rolfrider/countries/CountriesViewModel.kt
@@ -4,10 +4,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.rolfrider.countries.api.CountryFetcher
+import com.rolfrider.countries.api.CountryFetcherImpl
 
-class CountriesViewModel: ViewModel() {
+class CountriesViewModel(private val countryFetcher: CountryFetcher = CountryFetcherImpl()): ViewModel() {
 
-    private val countryFetcher = CountryFetcher()
+
 
     private val countryLiveData = MutableLiveData<List<CountryItem>>()
     private var allCountries: List<CountryItem> = emptyList()
---
+++ app/src/main/java/com/rolfrider/countries/api/CountryFetcherImpl.kt
@@ -0,0 +1,43 @@
+package com.rolfrider.countries.api
+
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class CountryFetcherImpl: CountryFetcher {
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(API_BASE_URL)
+        .client(HTTP_CLIENT)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val countryApi = retrofit.create(CountryApi::class.java)
+
+    override fun fetchAll(onSuccess: (List<Country>) -> Unit, onError: (String) -> Unit){
+        val call = countryApi.getAllCountries()
+        call.enqueue(object : Callback<List<Country>> {
+            override fun onFailure(call: Call<List<Country>>, t: Throwable) {
+                onError(t.message ?: "Unknown error")
+            }
+
+            override fun onResponse(call: Call<List<Country>>, response: Response<List<Country>>) {
+                if (response.isSuccessful){
+                    val countryList = response.body() ?: return onError("Expected response body")
+                    onSuccess(countryList)
+                }else{
+                    onError(response.message())
+                }
+            }
+        })
+    }
+
+
+    companion object {
+        const val API_BASE_URL = "https://restcountries.eu"
+        val HTTP_CLIENT: OkHttpClient by lazy { OkHttpClient() }
+    }
+}
